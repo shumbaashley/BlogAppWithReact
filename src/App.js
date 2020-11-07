c5a4ba8f29770c361f5b718f9ed8c176eacc25b6
shumbaashley <ashleytshumba@gmail.com>
2020-11-07 13:46:49 +0200

    added layout and auth pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import { Login } from './components/auth/Login';
+import { Register } from './components/auth/Register';
+import Landing from './components/layout/Landing'
+import Navbar from './components/layout/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <Switch>
+          <Route exact path="/" component={Landing}/>
+          <section className="container">
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/register" component={Register}/>
+          </section>
+        </Switch>
+
+      </Fragment>
+    </Router>
   );
 }
 
