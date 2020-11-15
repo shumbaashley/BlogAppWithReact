0c42cc75b9101025df8876bc9f7df93f4a8f0069
shumbaashley <ashleytshumba@gmail.com>
2020-11-15 16:37:18 +0200

    creating user profile successfully

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Landing from './components/layout/Landing'
 import Navbar from './components/layout/Navbar'
 import  Alert from './components/layout/Alert'
 import Dashboard from './components/dashboard/Dashboard'
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute'
 import {loadUser} from './actions/auth' 
 import setAuthToken from './utils/setAuthToken'
@@ -38,6 +39,7 @@ const  App = () => {
             <Route exact path="/login" component={Login}/>
             <Route exact path="/register" component={Register}/>
             <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+            <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
           </Switch>
         </section>
 
